8c2021174447302337f845158f79184a33dc0623
Sebastian Sauer <ssauer@posteo.de>
2019-10-11 11:05:24 +0200

    fun to install fom packages

---
+++ public/download/install_fom_packages.R
@@ -0,0 +1,49 @@
+
+
+
+install_fom_packages <- function(selection = "standard"){
+
+
+  # define needed packages:
+
+  pckgs_standard <- c("mosaic", "tidyverse", "sjmisc", "twitteR", "psych",
+                      "devtools", "tidytext", "lsr", "sjstats",
+                      "gapminder", "rnaturalearth", "corrr", "okcupiddata",
+                      "wordcloud", "corrplot", "janitor", "SnowballC",
+                      "tinytex", "paramtest")
+
+
+  pckgs_advanced <- c(pckgs_standard, "ISLR", "caret")
+
+
+
+  # define customized bundle of packages, depending on "selection":
+
+  selection_key <- c("standard", "full")
+  selection_value <- list(pckgs_standard, pckgs_advanced)
+  names(selection_value) <- selection_key
+
+
+  # determine the selected bundle of pckgs to be installed:
+
+  selected_bundle <- selection_value[[selection_key]]
+
+
+
+  # install missing packges:
+
+  pckgs_tobeinstalled <- selected_bundle[!(selected_bundle %in% installed.packages()[,"Package"])]
+  if(length(pckgs_tobeinstalled)) {
+    install.packages(pckgs_tobeinstalled)
+    } else {
+      cat("No packages were missing. No packages have been installed")}
+
+  if ("tinytex" %in% pckgs_tobeinstalled)
+    cat("After installing `tinytex`, don't forget to install tex by using this commend `tinytex::install_tinytex()`.\nCheck out here more details: https://yihui.name/tinytex/ \n")
+
+
+  cat("Consider updating your packages next by using `update.packagges()` \n")
+
+
+
+}
