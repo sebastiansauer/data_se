bd6b22610bfe5ee1830f66b31ca0845e0780450a
Sebastian Sauer <ssauer@posteo.de>
2019-11-11 13:25:33 +0100

    render

--- public/download/install_fom_packages.R
+++ public/download/install_fom_packages.R
@@ -1,32 +1,37 @@
 
 
 
-install_fom_packages <- function(selection = "standard"){
+install_fom_packages <- function(selection = "standard",
+                                 show_selection = FALSE,
+                                 show_pckgs_to_be_installed = FALSE){
 
 
   # define needed packages:
 
-  pckgs_standard <- c("mosaic", "tidyverse", "sjmisc", "twitteR", "psych",
+  pckgs_standard <- c("mosaic", "tidyverse")
+
+
+  pckgs_advanced <- c(pckgs_standard, "ISLR", "caret", "nlme", "lme4",
+                      "sjmisc", "twitteR", "psych",
                       "devtools", "tidytext", "lsr", "sjstats",
                       "gapminder", "rnaturalearth", "corrr", "okcupiddata",
                       "wordcloud", "corrplot", "janitor", "SnowballC",
                       "tinytex", "paramtest", "rnaturalearth", "countrycode",
-                      "effsize", "pwr", "rocr")
-
-
-  pckgs_advanced <- c(pckgs_standard, "ISLR", "caret", "nlme", "lme4")
+                      "effsize", "pwr", "ROCR", "foreign")
 
 
 
-  # define customized bundle of packages, depending on "selection":
+  # define customized bundle of packages, depending on "selection" (as type of dictionnary):
 
-  selection_key <- c("standard", "full")
+  selection_key <- c("standard", "advanced")
   selection_value <- list(pckgs_standard, pckgs_advanced)
   names(selection_value) <- selection_key
 
 
   if(!selection %in% selection_key)
     error("Error: invalid choice for parameter `selection`")
+
+  cat("Selected bundle includes the following packages: \n", selection_value[[selection]],"\n")
 
 
   # determine the selected bundle of pckgs to be installed:
@@ -35,9 +40,13 @@ install_fom_packages <- function(selection = "standard"){
 
 
 
+
   # install missing packges:
 
   pckgs_tobeinstalled <- selected_bundle[!(selected_bundle %in% installed.packages()[,"Package"])]
+
+  cat("The following packages will be installed:\n", pckgs_tobeinstalled, "\n")
+
   if(length(pckgs_tobeinstalled)) {
     install.packages(pckgs_tobeinstalled)
     } else {
@@ -48,6 +57,7 @@ install_fom_packages <- function(selection = "standard"){
 
 
   cat("Consider updating your packages next by using `update.packagges()`.\n")
+
 
 
 
