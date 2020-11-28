21a50a504d9d92c8a240352654b2708d6d87f53e
Sebastian Sauer <ssauer@posteo.de>
2020-11-28 08:23:29 +0100

    update logistics post

--- build-site-trying.R
+++ build-site-trying.R
@@ -1,5 +1,10 @@
 options(blogdown.server.timeout=240)
+
+library(blogdown)
 blogdown::hugo_build()  # no re-compiling
-blogdown::serve_site()
+blogdown::serve_site()  # serve the website locally only
 
 blogdown::build_site()
+
+
+blogdown::stop_server()
